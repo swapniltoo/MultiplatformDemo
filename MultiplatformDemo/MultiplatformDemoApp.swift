0b15eacc6fe1548dbfc5e81697c36d614a24dfcd
swapniltoo <swapniltoo@gmail.com>
2023-04-03 00:29:54 +0530

    Initial Commit

---
+++ MultiplatformDemo/MultiplatformDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformDemoApp.swift
+//  MultiplatformDemo
+//
+//  Created by Swapnil Khollam on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
